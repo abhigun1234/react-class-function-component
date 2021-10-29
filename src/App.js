be158453f772e58f8d4a4a89adffa72e5861741c
Abhishek <abhi.ghosh27feb@gmail.com>
2021-10-29 21:41:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -12,76 +12,61 @@ import Person from './Person';
 function App() {
   return (
     <div className="App">
-      <div className="header">
-    
-        <label>My Restro</label>
-        <div class="mycontainer">
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Butter Chicken</h5>
-          <img src="https://thumbs.dreamstime.com/b/butter-chicken-curry-coriander-naan-bread-89411506.jpg"/>
-          <p class="card-text">Butter chicken </p>
-          <p class="card-text">price 234 rs </p>
-          <a href="#" class="btn btn-primary">Add To cart </a>
-        </div>
-        </div>
-        </div>
-      </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">My restro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      
+      </ul>
     </div>
+  </div>
+</nav>
+<div class="card" >
+  <img class="imgMenu" src="https://www.simplyrecipes.com/thmb/1SXZ_F1GC6ww_ppWnrdbKgHi9fQ=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" >
+  <img class="imgMenu" src="https://www.simplyrecipes.com/thmb/1SXZ_F1GC6ww_ppWnrdbKgHi9fQ=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" >
+  <img class="imgMenu" src="https://www.simplyrecipes.com/thmb/1SXZ_F1GC6ww_ppWnrdbKgHi9fQ=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" >
+  <img class="imgMenu" src="https://www.simplyrecipes.com/thmb/1SXZ_F1GC6ww_ppWnrdbKgHi9fQ=/2000x1333/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
   );
 }
 
